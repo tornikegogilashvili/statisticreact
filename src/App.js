077573f18eb8dd6e95243cca44a32dbd0f64f316
tornikegogilashvili <tornikegogilashvili@yahoo.com>
2023-07-08 13:34:18 +0400

    styles

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import { Route, Routes } from 'react-router-dom';
 import routes from "../src/conf/constants/routes"
 import './App.css';
 import { Header } from './components/Header';
+import { USDA } from './components/USDA';
 
 function App() {
   return (
     <div>
       <Header  />
+      <USDA />
       {/* <Routes>
         <Route  />
       </Routes> */}
