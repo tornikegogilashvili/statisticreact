d9eef7aa4e80d08cd7751773fd96ffecc2f63866
tornikegogilashvili <tornikegogilashvili@yahoo.com>
2023-07-13 22:49:19 +0400

    html changes

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,36 +1,40 @@
 import React from "react"
 import "./cards.css"
 import მცენარე from "../images/მცენარე.png"
-
+import მეცხოველეობა from "../images/ძროხა (1).png"
+import აკვაკულტურა from "../images/თევზი.png"
+import სასურსათობალანსი from "../images/სურსათი.png"
+import ძირითადიინფო from "../images/ძირითადი ინფო.png"
 
 export const Cards = () => {
     return(
     <div className="cards_div">
         <div className="cards_div_white">
-            <div className="img_div"><img src={მცენარე} /></div>
+            <div className="img_div"><img src={მცენარე} alt="მემცენარეობა" /></div>
             <h3>მემცენარეობა</h3>
             <p>ნახვა</p>
         </div>
         <div className="cards_div_white">
-            <div className="img_div"><img src={მცენარე} /></div>
+            <div className="img_div"><img src={მეცხოველეობა} alt="მეცხოველეობა" /></div>
             <h3>მემცენარეობა</h3>
             <p>ნახვა</p>
         </div>
         <div className="cards_div_white">
-            <div className="img_div"><img src={მცენარე} /></div>
+            <div className="img_div"><img src={აკვაკულტურა} alt="აკვაკულტურა" /></div>
             <h3>მემცენარეობა</h3>
             <p>ნახვა</p>
         </div>
         <div className="cards_div_white">
-            <div className="img_div"><img src={მცენარე} /></div>
+            <div className="img_div"><img src={სასურსათობალანსი} alt="სასურსათობალანსი" /></div>
             <h3>მემცენარეობა</h3>
             <p>ნახვა</p>
         </div>
         <div className="cards_div_white">
-            <div className="img_div"><img src={მცენარე} /></div>
+            <div className="img_div"><img src={ძირითადიინფო} alt="ძირითადიინფო" /></div>
             <h3>მემცენარეობა</h3>
             <p>ნახვა</p>
         </div>
+        <div className="line_div"></div>
     </div>
     )
 
