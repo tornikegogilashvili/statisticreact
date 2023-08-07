9faf3a957ce427709ec1565ab15b43fa8cf97bef
tornikegogilashvili <tornikegogilashvili@yahoo.com>
2023-08-07 16:06:09 +0400

    background dark mode. active dots

--- src/components/BackgroundSlider.js
+++ src/components/BackgroundSlider.js
@@ -1,81 +1,65 @@
-import React, { useEffect, useState } from "react"
-import "./backgroundSlider.css"
-import imageSlide from "./data"
-import { Cards } from "./Cards"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons'
-import { Card } from "./Card"
-
+import React, { useEffect, useState } from "react";
+import "./backgroundSlider.css";
+import imageSlide from "./data";
+import { Cards } from "./Cards";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { Card } from "./Card";
 
 const BackgroundSlider = () => {
-    const [currentState, setCurrentState] = useState(0);
-    useEffect(() => {
-        console.log("first", currentState);
-        if(currentState==7){
-            setCurrentState(0)
-        }else if(currentState==-1){
-            setCurrentState(6)
-        }
-        const timer = setTimeout(() => {
-            if(currentState==6){
-                setCurrentState(0)
-            }else{
-                setCurrentState(currentState+1)
-            }
-        },5000)
-        return () => clearTimeout(timer)
-    },[currentState])
+  const [currentState, setCurrentState] = useState(0);
 
-    document.body.style.backgroundImage = `url(${imageSlide[currentState]?.url})`;
-    document.body.style.backgroundPosition = "center";
-    document.body.style.backgroundSize = "cover";
-    document.body.style.backgroundRepeat = "no-repeat";
-    document.body.style.backgroundAttachment = "fixed";
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setCurrentState((prevState) => (prevState + 1) % imageSlide.length);
+    }, 5000);
 
-    const goToNext = (currentState) => {
-        setCurrentState(currentState)
-    }
-    const previous = (currentState) => {
-            setCurrentState(currentState)
-    }
-    const next = (currentState) => {
-        setCurrentState(currentState)
-    }
-    // useEffect(() => {
-    //     console.log("second", currentState);
-    //     if(currentState==6){
-    //         setCurrentState(0)
-    //     }else{
-    //         setCurrentState(currentState-1)
-    //     }
-    // },)
+    return () => clearTimeout(timer);
+  }, [currentState]);
 
-    return (
-        <div className="container_style">
-            <div className="bgImageStyle" ></div>
-            <div className="description">
-                <div>
-                    <h2>სოფლის მეურნეობის <br/> სტატისტიკის პორტალი</h2>
-                    <Cards />
-                    <p className="description_p">{imageSlide[currentState]?.title}</p>
-                    <FontAwesomeIcon onClick={()=>previous(currentState-1)} className="slide_arrow" icon={faArrowLeft} />
-                    <FontAwesomeIcon onClick={()=>next(currentState+1)}  className="slide_arrow" icon={faArrowRight} />
-                    <Card currentState={currentState} />
-                </div>
-                <div className="carousel-boullt">
-                    {
-                        imageSlide.map((imageSlide, currentState) => (
-                            <span  className="carousel_span" key={currentState} onClick={()=> {goToNext(currentState)}}  >
+  const goToNext = () => {
+    setCurrentState((prevState) => (prevState + 1) % imageSlide.length);
+  };
 
-                            </span>
-                        ))
-                    }
-                </div>
-                <p className="footer_p">2023 საქართველოს სტატისტიკის ეროვნული სამსახური
-<br/><a href="#">მონაცემთა გამოყენების პირობები</a></p>
-            </div>
+  const goToPrevious = () => {
+    setCurrentState((prevState) => (prevState - 1 + imageSlide.length) % imageSlide.length);
+  };
+
+  const goToSlide = (index) => {
+    setCurrentState(index);
+  };
+
+  return (
+    <div className="container_style">
+      <div className="bgImageStyle">
+        <div
+          className="backgroundImage"
+          style={{ backgroundImage: `url(${imageSlide[currentState]?.url})` }}
+        ></div>
+        <div className="darkOverlay"></div>
+      </div>
+      <div className="description">
+        <div>
+          <h2>სოფლის მეურნეობის<br />სტატისტიკის პორტალი</h2>
+          <Cards />
+          <p className="description_p">{imageSlide[currentState]?.title}</p>
+          <FontAwesomeIcon onClick={goToPrevious} className="slide_arrow" icon={faArrowLeft} />
+          <FontAwesomeIcon onClick={goToNext} className="slide_arrow" icon={faArrowRight} />
+          <Card currentState={currentState} />
         </div>
-    )
-}
+        <div className="carousel-boullt">
+          {imageSlide.map((slide, index) => (
+            <span
+              className={`carousel_span ${currentState === index ? 'active' : ''}`}
+              key={index}
+              onClick={() => goToSlide(index)}
+            ></span>
+          ))}
+        </div>
+        <p className="footer_p">2023 საქართველოს სტატისტიკის ეროვნული სამსახური<br /><a href="#">მონაცემთა გამოყენების პირობები</a></p>
+      </div>
+    </div>
+  );
+};
 
-export default BackgroundSlider
+export default BackgroundSlider;
